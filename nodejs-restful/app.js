d4aaaddb37326365d02b7e065d4e302835c96664
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-16 05:17:34 +0700

    Completed item 17 OS Module.

--- nodejs-restful/app.js
+++ nodejs-restful/app.js
@@ -1,7 +1,8 @@
 
-const path = require('path')
+const os = require('os')
 
-var pathObj = path.parse(__filename)
+const totalMemory = os.totalmem()
+const freeMemory = os.freemem()
 
-console.log(pathObj)
-
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
