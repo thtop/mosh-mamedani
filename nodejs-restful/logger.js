3a35043b8affa1d70569ec075aee160484cf5506
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-16 04:49:32 +0700

    Complted item 14 Loading a Module.

--- nodejs-restful/logger.js
+++ nodejs-restful/logger.js
@@ -1,9 +1,9 @@
-var url = 'http://mylogger.io/log'
+var url = 'http://mylogger.io/log';
 
 function log(message) {
     // Send an HTTP request
-    console.log(message)
+    console.log(message);
 }
 
-module.exports.log
+module.exports = log;
 //module.exports.endPoint = url
