400354444614d5d3587ee95270323a67705a1bc2
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-16 05:48:45 +0700

    Completed item 21 Extending Event Emitter.

--- nodejs-restful/app.js
+++ nodejs-restful/app.js
@@ -1,13 +1,12 @@
 
 const EventEmitter = require('events')
-const emitter = new EventEmitter()
+
+const Logger = require('./logger')
+const logger = new Logger()
 
 // Register a listener
-emitter.on('messageLogged', (arg) => {
+logger.on('messageLogged', (arg) => {
     console.log('Listener called', arg)
 })
 
-// Raise an event
-emitter.emit('messageLogged', {id: 1, url: 'http://'})
-
-// Raise: logging (data: messge)
+logger.log('message')
