50d1027fa2a843410ec31b4e520e867845306f66
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-16 05:38:53 +0700

    Completed item 20 Event Arguments.

--- nodejs-restful/app.js
+++ nodejs-restful/app.js
@@ -3,10 +3,11 @@ const EventEmitter = require('events')
 const emitter = new EventEmitter()
 
 // Register a listener
-emitter.on('messageLogged', function() {
-    console.log('Listener called')
+emitter.on('messageLogged', (arg) => {
+    console.log('Listener called', arg)
 })
 
-// emit : Making a noise, produce - signalling
 // Raise an event
-emitter.emit('messageLogged')
+emitter.emit('messageLogged', {id: 1, url: 'http://'})
+
+// Raise: logging (data: messge)
