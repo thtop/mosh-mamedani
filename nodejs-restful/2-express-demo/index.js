95b74552fc638da24d76e43c6a1004474fd14dd5
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-16 13:14:48 +0700

    Finished item 48 Handling HTTP GET Requests.

--- nodejs-restful/2-express-demo/index.js
+++ nodejs-restful/2-express-demo/index.js
@@ -1,26 +1,28 @@
 const express = require('express')
 const app = express()
 
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'}
+]
+
 app.get('/', (req, res) => {
     res.send('Hello World!!!')
 })
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3])
+    res.send(courses)
 })
 
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id)
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+
+    if (!course) res.status(404).send('The course with the given ID was not found.')
+
+    res.send(course)
 })
 
-// app.get('/api/posts/:year/:month', (req, res) => {
-//     res.send(req.params)
-// })
-
-// http://localhost:3000/api/posts/2019/1?sortBy=name
-app.get('/api/posts/:year/:month', (req, res) => {
-    res.send(req.query)
-})
 
 const port = process.env.PORT || 3000
 app.listen(3000, () => console.log(`Listening on port ${port}`))
