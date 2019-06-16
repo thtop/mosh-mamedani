4aaa832771ebe5c5a4bb9c7fd809a4cd24dc67d0
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-16 13:07:11 +0700

    Finished item 47 Route Parameters.

--- nodejs-restful/2-express-demo/index.js
+++ nodejs-restful/2-express-demo/index.js
@@ -9,6 +9,18 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3])
 })
 
-// PORT
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id)
+})
+
+// app.get('/api/posts/:year/:month', (req, res) => {
+//     res.send(req.params)
+// })
+
+// http://localhost:3000/api/posts/2019/1?sortBy=name
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.query)
+})
+
 const port = process.env.PORT || 3000
 app.listen(3000, () => console.log(`Listening on port ${port}`))
