0bec4fcb94ad07ed8353c29058d4f39a9c967646
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-18 06:48:35 +0700

    Finished item 72 Callbacks.

--- nodejs-restful/3-async-demo/index.js
+++ nodejs-restful/3-async-demo/index.js
@@ -1,17 +1,30 @@
 console.log('Before')
 
-const user = getUser(1)
-console.log(user) //undefined
+// Callbacks
+const user = getUser(1, (user) => {
+    console.log('User: ', user)
+
+    // Get the repositiories
+    getRepositories(user.getRepositories, repo => {
+        console.log('Repo: ', repo)
+    })
+})
 
 console.log('After')
 
-// Callbacks
 // Promises
 // Async/await
 
-function getUser(id) {
+function getUser(id, callback) {
     setTimeout(() => {
         console.log('Reading a use from a database...')
-        return { id: id, gitHubUserbane: 'Thamonwan'}
+        callback({ id: id, gitHubUserbane: 'Thamonwan'})
+    }, 2000)
+}
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        console.log('Calling GitHub API...')
+        callback(['repo1', 'repo2', 'repo3'])
     }, 2000)
 }
