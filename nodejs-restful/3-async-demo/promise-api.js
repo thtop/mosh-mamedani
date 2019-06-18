85ce027b56e9e123df5035edbcfa3b05e83c5675
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-18 07:37:11 +0700

    Finished item 79 Running Parallel Promises.

--- nodejs-restful/3-async-demo/promise-api.js
+++ nodejs-restful/3-async-demo/promise-api.js
@@ -1,6 +1,19 @@
 
-// const p = Promise.resolve({ id: 1})
-// p.then(result => console.log(result))
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('Async operation 1...')
+        resolve(1)
+        //reject(new Error('because something failed.'))
+    }, 2000)
+})
 
-const p = Promise.reject(new Error('reasom for rejection...'))
-p.catch(err => console.log(err))
+const p2 = new Promise(resolve => {
+    setTimeout(() => {
+        console.log('Async operation 2...')
+        resolve(2)
+    }, 2000)
+})
+
+Promise.race([p1, p2])
+ .then(result => console.log(result))
+ .catch(err => console.log('Error: ', err.message))
