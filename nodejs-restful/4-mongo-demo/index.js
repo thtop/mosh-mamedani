404e4b10e1654dac91e1dcd1015e2facfc474de5
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-19 21:52:05 +0700

    Finished Regular Expressions.

--- nodejs-restful/4-mongo-demo/index.js
+++ nodejs-restful/4-mongo-demo/index.js
@@ -28,14 +28,18 @@ async function createCourse() {
 
 async function getCourses() {
 
-    // or
-    // and
-
     const courses = await Course
         //.find({ author: 'Thamonwan', isPublished: true })
-        .find()
-        .or([ {author: 'Thamonwan'}, { isPublished: true } ])
-        .and([ ])
+
+        // Starts with Thamon
+        .fing({ author: /^Thamon/ })
+
+        // Ends with Maneechan
+        .find({ author: /Maneechan$/i })
+
+        // Contains wan
+        .find({ author: /.*wan.*/i })
+
         .limit(10)
         .sort({ name: 1 })
         .select({ name: 1, tags: 1 });
