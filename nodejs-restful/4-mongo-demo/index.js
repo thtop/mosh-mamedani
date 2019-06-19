6554c3aba287d194180f687fb5bae0388528fc0b
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-19 21:36:02 +0700

    Finished item 89 Querying Documents.

--- nodejs-restful/4-mongo-demo/index.js
+++ nodejs-restful/4-mongo-demo/index.js
@@ -26,4 +26,13 @@ async function createCourse() {
     console.log(result);
 }
 
-createCourse();
+async function getCourses() {
+    const courses = await Course
+        .find({ author: 'Thamonwan', isPublished: true })
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1, tags: 1 });
+    console.log(courses);
+}
+
+getCourses();
