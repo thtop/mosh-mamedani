a470b3e229fd5a3c97a012a0017b3abaa977236a
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-19 21:43:02 +0700

    Finished item 90 Comparison Qurey Operations.

--- nodejs-restful/4-mongo-demo/index.js
+++ nodejs-restful/4-mongo-demo/index.js
@@ -27,8 +27,20 @@ async function createCourse() {
 }
 
 async function getCourses() {
+
+    // eq (equal)
+    // ne (not equal)
+    // gt (greater than)
+    // gte (greater than or equal to)
+    // lt (less than)
+    // lte (less than or equal to)
+    // in
+    // nin (not in)
+
     const courses = await Course
-        .find({ author: 'Thamonwan', isPublished: true })
+        //.find({ author: 'Thamonwan', isPublished: true })
+        //.find({ price: { $gt: 10, $lte: 20 } })
+        .find({ price: { $in: [10, 15, 20] } })
         .limit(10)
         .sort({ name: 1 })
         .select({ name: 1, tags: 1 });
