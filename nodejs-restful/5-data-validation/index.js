5453565a044c97674ae018a7b0dc6be3a7fc2152
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-20 21:33:35 +0700

    Finished item 104 Custom Validators.

--- nodejs-restful/5-data-validation/index.js
+++ nodejs-restful/5-data-validation/index.js
@@ -18,7 +18,15 @@ const courseSchema = new mongoose.Schema({
         enum: ['web', 'mobile', 'network']
     },
     author: String,
-    tags: [ String ],
+    tags: {
+        type: Array,
+        validate: {
+            validator: function(v) {
+                return v && v.length > 0;
+            },
+            message: 'A course should have at least one tag.'
+        }
+    },
     date: { type: Date, default: Date.now },
     isPublished: Boolean,
     price: {
@@ -34,9 +42,9 @@ const Course = mongoose.model('Course', courseSchema);
 async function createCourse() {
     const course = new Course({
         name: 'Angular Course',
-        category: '-',
+        category: 'web',
         author: 'Baramy',
-        tags: ['angular', 'frontend'],
+        tags: null,
         isPublished: true,
         price: 15
     });
